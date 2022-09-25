a9e73e34468a4626be1188aa9d989e0c6f5ce4fc
Abdul Aziz (A575072) <seeazee@gmail.com>
2022-09-25 14:14:33 +0100

    Update createAndMergePr.groovy

--- vars/createAndMergePr.groovy
+++ vars/createAndMergePr.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
-def call(String base = 'main', String  branch = 'bau-branch') {
-  echo "Create PR, ${branch} to ${base} "
+def call(String  branch = 'bau-branch') {
+  echo "Create PR, ${branch}"
 }
